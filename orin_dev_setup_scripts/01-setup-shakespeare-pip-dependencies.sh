f7c1b7d792eb26dc24b1e9602bb5cfc8cf0a1a2e
Meapy011 <meap09121@gmail.com>
2026-02-11 11:15:15 -0800

    Commented out venv

--- orin_dev_setup_scripts/01-setup-shakespeare-pip-dependencies.sh
+++ orin_dev_setup_scripts/01-setup-shakespeare-pip-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python3.10-venv -y
-cd ..
-python3 -m venv venv
-source venv/bin/activate
-sudo apt install python-pip -y
-pip install sentencepiece tiktoken tqdm rich torchinfo plotly seaborn tensorboard pyyaml
+#sudo apt install python3.10-venv -y
+#cd ..
+#python3 -m venv venv
+#source venv/bin/activate
+sudo apt install python-pip python3-pip -y
+pip install sentencepiece tiktoken tqdm rich torchinfo plotly seaborn tensorboard pyyaml textual
 pip install "triton==3.5.1"
 echo "After you can run bash data/shakespeare_char/get_dataset.sh"
