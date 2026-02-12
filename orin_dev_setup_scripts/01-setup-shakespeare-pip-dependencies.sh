8ad3469064acfa4cc470697f2534cbbf0e5edf97
Meapy011 <meap09121@gmail.com>
2026-02-12 12:42:12 -0800

    Added color and notes

--- orin_dev_setup_scripts/01-setup-shakespeare-pip-dependencies.sh
+++ orin_dev_setup_scripts/01-setup-shakespeare-pip-dependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+## Update Apt list and install python-pip
 sudo apt update
-#sudo apt install python3.10-venv -y
-#cd ..
-#python3 -m venv venv
-#source venv/bin/activate
 sudo apt install python-pip python3-pip -y
+
+## Install pip dependencies
 pip install sentencepiece tiktoken tqdm rich torchinfo plotly seaborn tensorboard pyyaml textual
 pip install "triton==3.5.1"
-echo "After you can run bash data/shakespeare_char/get_dataset.sh"
+
+## Get dataset
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+echo -e "${RED}After you can run bash data/shakespeare_char/get_dataset.sh"
+
